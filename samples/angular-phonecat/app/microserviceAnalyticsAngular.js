be49051e43b1a2661c54a674bfe172b1b87423e3
James Randall <james@accidentalfish.com>
2016-03-27 13:12:04 +0100

    Added sample, updated dependency

---
+++ samples/angular-phonecat/app/microserviceAnalyticsAngular.js
@@ -0,0 +1,55 @@
+// JavaScript source code
+// AngularJS support
+(function (analyticsInstance) {
+    var global = this;
+    if (global.angular) {
+        var microserviceAnalyticsModule = global.angular.module('microserviceAnalytics', []);
+        microserviceAnalyticsModule.provider("$exceptionHandler", {
+            $get: function (accidentalFishExceptionLoggingService) {
+                return accidentalFishExceptionLoggingService;
+            }
+        });
+        microserviceAnalyticsModule.factory("accidentalFishExceptionLoggingService", ['$log', '$window', function ($log) {
+            function error(exception, cause) {
+                $log.error.apply($log, arguments);
+                analyticsInstance.handleJavaScriptError(exception, cause);
+            }
+
+            return error;
+        }]);
+        microserviceAnalyticsModule.directive('microserviceAnalytics', [function() {
+            var definition = {
+                restrict: 'E',
+                replace: false,
+                scope: {
+                    propertyId: '@',
+                    propertyKey: '@',
+                    uploadIntervalMs: '=',
+                    correlationEnabled: '=',
+                    collectionEndpoint: '@',
+                    correlationIdKey: '@',
+                    correlationIdPrefix: '@',
+                    autoStartJourneys: '=',
+                    httpBlacklist: '@',
+                    httpWhitelist: '@',
+                    userIdProvider: '&?',
+                    userIdKey: '@',
+                    sessionIdProvider: '&?',
+                    sessionIdKey: '@'
+                }
+            };
+            
+            function init() {
+                analyticsInstance.configure()
+            };
+            
+            definition.link = function(scope, element, attrs) {
+                scope.$watch('propertyId', function() {
+                    analyticsInstance.configure(scope);
+                });
+            };
+            
+            return definition;
+        }]);
+    }
+}).call(this, this.microserviceAnalytics);
